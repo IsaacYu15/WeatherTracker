1f5785cf4469dcf12a2f9bd08a071f964e3c2def
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-08-17 21:01:28 -0400

    chore: reorder scripts

--- src/App.js
+++ src/App.js
@@ -1,67 +1,13 @@
 import Map from './Maps.js';
-import React, { useState, useEffect } from 'react';
-import Card from './Card.js';
-
+import './App.css';
 
 function App() {
-
-
-  //look into states and hooks documentatino provided by react
-  const [lat, setLat] = useState([]);
-  const [long, setLong] = useState([]);
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async() => {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        setLat(position.coords.latitude);
-        setLong(position.coords.longitude);
-      })
-
-      //get api, and store as a JSON- >look into JSONS
-      await fetch("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&APPID="+process.env.REACT_APP_API_KEY, {
-      method: 'POST',  
-      mode: 'cors'
-      })
-      .then(response => {
-        return response.json()
-      })
-      .then(data => {
-        setData(data)
-      })
-    }
-
-    fetchData();
-
-
-  }, [lat, long]); //why?
-
-
-
 
   return (
 
     <div className="App">
-      
-      {(typeof data.main != 'undefined') ? (
-        <Card data={data}/>
-      ): (
-        <div></div>
-      )}
-      
 
-      {(typeof data.main != 'undefined') ? (
-         <Map apikey={process.env.REACT_APP_API_KEY_HEREMAPS} newLat={lat} newLong={long} />
-
-      ): (
-        <div></div>
-      )}
-
-
-
-    
-
-
+      <Map apikey={process.env.REACT_APP_API_KEY_HEREMAPS}/>
 
     </div>
 
