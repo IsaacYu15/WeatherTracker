7976945b83a355b8ff6c25f89c6eab002ff6521b
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-08-19 16:06:23 -0400

    feat: set up hero section

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
 
     <div className="App">
 
+      <div className="Hero">
+        <h1>WEATHER APP</h1>
+        <p>Click on the map, based on the latitude and longitude  you will be able to see weather stastics!</p>
+      </div>
+
       <Map apikey={process.env.REACT_APP_API_KEY_HEREMAPS}/>
 
     </div>
